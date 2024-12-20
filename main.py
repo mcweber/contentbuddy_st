1aa03c6eb62d6a78a7bfdbe21a4115cd034c1d1b
Martin <webermac@proton.me>
2024-12-20 12:34:23 +0100

    bug fixes

--- main.py
+++ main.py
@@ -1,9 +1,3 @@
-# ---------------------------------------------------
-# Author: M. Weber
-# ---------------------------------------------------
-# 20.12.2024 added code authentication
-# ---------------------------------------------------
-
 from datetime import datetime
 import streamlit as st
 import ask_llm
@@ -11,20 +5,23 @@ import ask_doc
 import scrape_web
 import prompts
 
+import os
 from dotenv import load_dotenv
 load_dotenv()
 
 # Define Constants ---------------------------------------------------
 VERSION = "20.12.2024"
+
 HEUTE = str(datetime.now().date())
 AUSGABE_SPRACHE = ["DEU", "ENG", "FRA"]
-LLM = "gpt4o" # ONLINE: gpt4o, gpt4omini, llama3 | LOCAL: llama3, mistral
+LLMS = ["gemini", "gpt4o", "gpt4omini", "llama"]
+# LLM = "gemini" # ONLINE: gemini, gpt4o, gpt4omini, llama | LOCAL: llama, mistral
 LOCAL = False
 
 # Functions -------------------------------------------------------------
 @st.dialog("Login")
-def login_user_dialog() -> None:
-    with st.form(key="loginForm"):
+def login_code_dialog() -> None:
+    with st.form(key="code_form"):
         code = st.text_input(label="Code", type="password")
         if st.form_submit_button("Enter"):
             if code == os.environ.get('CODE'):
@@ -35,9 +32,25 @@ def login_user_dialog() -> None:
 
 # Main -----------------------------------------------------------------
 def main() -> None:
+    st.set_page_config(page_title='ContentBuddy', layout='wide')
+
+    # Initialize Session State -----------------------------------------
+    if 'init' not in st.session_state:
+        st.session_state.init: bool = True
+        st.session_state.code: bool = False
+        st.session_state.eingabe: str = ""
+        st.session_state.ausgabe: str = ""
+        st.session_state.model_idx: int = 0
+        st.session_state.search_status: bool = False
+        st.session_state.system_prompt: str = prompts.get_system_prompt()
+        st.session_state.format_prompt: str = ""
+        st.session_state.ausgabe_sprache_idx: int = 0
+
+    # Authentication ---------------------------------------------------
+    if not st.session_state.code:
+        login_code_dialog()
 
     # Initialize screen sections -------------------------------------------
-    st.set_page_config(page_title='ContentBuddy', layout='wide')
     st.subheader("ContentBuddy")
 
     col_left, col_mid, col_right = st.columns([4, 1, 4])
@@ -54,25 +67,8 @@ def main() -> None:
         st.subheader("AUSGABE")
 
     # with footer:
-    st.write(f"Version: {VERSION} M. Weber | Status: POC | LLM: {LLM} | Lokal: {LOCAL}")
-
-    # Initialize Session State -----------------------------------------
-    if 'init' not in st.session_state:
-        st.session_state.init: bool = True
-        st.session_state.code: bool = False
-        st.session_state.eingabe: str = ""
-        st.session_state.ausgabe: str = ""
-        # st.session_state.schlagworte: str = ""
-        st.session_state.search_status: bool = False
-        st.session_state.system_prompt: str = prompts.SYSTEM_PROMPT
-        st.session_state.format_prompt: str = ""
-        st.session_state.zielformat: str = ""
-        st.session_state.ausgabe_sprache_idx: int = 0
-
-    # Authentication ---------------------------------------------------
-    if not st.session_state.code:
-        login_user_dialog()
-
+    st.write(f"Version: {VERSION} M. Weber | Status: POC | LLM: {LLMS[st.session_state.model_idx]} | Lokal: {LOCAL}")
+    
     # Define Input Form ----------------------------------------------
     with container_left:
 
@@ -103,47 +99,39 @@ def main() -> None:
         eingabe_text = st.text_area(label="Was sind die Quelltexte?", value=value_text, height=500)
         if eingabe_text != value_text:
             st.session_state.eingabe = eingabe_text
-            # st.session_state.search_status = False
 
         # Reset Button ------------------------------------------------------
         if st.button("Reset"):
             st.session_state.eingabe = ""
             st.session_state.ausgabe = ""
             st.session_state.schlagworte = ""
-            # st.session_state.search_status = False
             st.rerun()
 
     # Define Parameter Form ----------------------------------------------
     with container_mid:
-        if st.button("Fachartikel"):
-            st.session_state.zielformat = "Fachartikel"
-            st.session_state.format_prompt = prompts.FACHARTIKEL
-            st.session_state.search_status = True
-        if st.button("Blogbeitrag"):
-            st.session_state.zielformat = "Blogbeitrag"
-            st.session_state.format_prompt = prompts.BLOGBEITRAG
-            st.session_state.search_status = True
-        if st.button("Social Media Post"):
-            st.session_state.zielformat = "Social Media Post"
-            st.session_state.format_prompt = prompts.SOCIAL_MEDIA_POST
-            st.session_state.search_status = True
-        if st.button("Schlagworte"):
-            st.session_state.zielformat = "Schlagworte"
-            st.session_state.format_prompt = prompts.SCHLAGWORTE
-            st.session_state.search_status = True
 
-        sprache = AUSGABE_SPRACHE[st.session_state.ausgabe_sprache_idx]
-        index = st.session_state.ausgabe_sprache_idx
+        # Choose Prompt -----------------------------------------------------
+        for item in prompts.get_prompt_names():
+            if st.button(item):
+                st.session_state.format_prompt = prompts.get_prompt_by_name(item)
+                st.session_state.search_status = True
 
-        ausgabe_sprache_neu = st.radio(f"Ausgabe-Sprache ({sprache})", AUSGABE_SPRACHE, index=index)
-        if ausgabe_sprache_neu != sprache:
+        # Choose LLM & Language ---------------------------------------------
+        ausgabe_sprache_neu = st.radio("Ausgabe-Sprache", AUSGABE_SPRACHE, index=st.session_state.ausgabe_sprache_idx)
+        if ausgabe_sprache_neu != AUSGABE_SPRACHE[st.session_state.ausgabe_sprache_idx]:
             st.session_state.ausgabe_sprache_idx = AUSGABE_SPRACHE.index(ausgabe_sprache_neu)
+            st.rerun()
+
+        model_neu = st.radio("Modell", LLMS, index=st.session_state.model_idx)
+        if model_neu != LLMS[st.session_state.model_idx]:
+            st.session_state.model_idx = LLMS.index(model_neu)
             st.rerun()
 
     # Execute Search & Display Ausgabe -------------------------------------------
     if st.session_state.search_status and st.session_state.eingabe:
         prompt = f"#{AUSGABE_SPRACHE[st.session_state.ausgabe_sprache_idx]}\n\n {st.session_state.format_prompt}"
-        llm_handler = ask_llm.LLMHandler(llm=LLM, local=LOCAL)
+        llm_handler = ask_llm.LLMHandler(llm=LLMS[st.session_state.model_idx], local=LOCAL)
+        
         st.session_state.ausgabe = llm_handler.ask_llm(
             temperature=0.2,
             question=prompt,
@@ -152,9 +140,10 @@ def main() -> None:
             # web_results_str=st.session_state.web_results,
             source_doc_str=st.session_state.eingabe,
             )
+        
         with container_right:
             st.write(st.session_state.ausgabe)
-        st.session_state.zielformat = ""
+        
         st.session_state.search_status = False
 
 if __name__ == "__main__":
